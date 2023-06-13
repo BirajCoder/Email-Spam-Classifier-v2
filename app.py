e14e18542cb59dd60586e732c9355d9e330bff11
Biraj <biraj@jovian.com>
2023-06-13 20:02:13 +0530

    Created API endpoint

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, request
-import pickle
-
+from flask import Flask, render_template, request, jsonify
+from utils import model_predict
 app = Flask(__name__)
-cv = pickle.load(open("models/cv.pkl", 'rb'))
-clf = pickle.load(open("models/clf.pkl", 'rb'))
+
 
 @app.route("/")
 def home():
@@ -12,13 +10,17 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == "POST":
-        email = request.form.get('content')
-    tokenized_email = cv.transform([email]) # X 
-    prediction = clf.predict(tokenized_email)
-    # If the email is spam prediction should be 1
-    prediction = 1 if prediction == 1 else -1
+    email = request.form.get('content')
+    prediction = model_predict(email)
     return render_template("index.html", prediction=prediction, email=email)
+
+# Create an API endpoint
+@app.route('/api/predict', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)  # Get data posted as a json
+    email = data['content']
+    prediction = model_predict(email)
+    return jsonify({'prediction': prediction, 'email': email})  # Return prediction
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
